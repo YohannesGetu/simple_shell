b1ff1f2825c5e8be6d6d9892d6cafe6e32263d54
samuel ogoigbe <ogoigbe12@gmail.com>
2021-08-18 05:42:34 -0700

    update to the shell repo

--- builtins2.c
+++ builtins2.c
@@ -85,3 +85,35 @@ char *add_value(vars_t *vars)
 	new[i + 1 + j] = '\0';
 	return (new);
 }
+
+/**
+ * _atoi - converts a string into an integer
+ * @str: string to convert
+ *
+ * Return: the integer value, or -1 if an error occurs
+ */
+int _atoi(char *str)
+{
+	unsigned int i, digits;
+	int num = 0, num_test;
+
+	num_test = INT_MAX;
+	for (digits = 0; num_test != 0; digits++)
+		num_test /= 10;
+	for (i = 0; str[i] != '\0' && i < digits; i++)
+	{
+		num *= 10;
+		if (str[i] < '0' || str[i] > '9')
+			return (-1);
+		if ((i == digits - 1) && (str[i] - '0' > INT_MAX % 10))
+			return (-1);
+		num += str[i] - '0';
+		if ((i == digits - 2) && (str[i + 1] != '\0') && (num > INT_MAX / 10))
+			return (-1);
+	}
+	if (i > digits)
+		return (-1);
+	printf("%d\n", num);
+	return (num);
+}
+
